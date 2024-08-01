b7f93c53c65464d42226893035c21406c9e97f4e
Matias Zwicker <matizwicker95@gmail.com>
2024-08-01 13:51:36 -0300

    termine el tp

--- config/db.js
+++ config/db.js
@@ -1,19 +1,16 @@
-
+// Mongo DB Atlas connection
 import { connect } from "mongoose";
 
-// Since V 6 this is no longer neccesary
-// const options = {
-//   maxPoolSize: 100,
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// };
-
 const db_uri = process.env.db_uri;
+
+if (!db_uri) {
+  throw new Error('db_uri no está definida en las variables de entorno');
+}
+
 async function main() {
   await connect(db_uri);
 }
+
 main()
-  .then(() =>
-    console.log("MongoDB Database on Mongo Atlas Cloud Service Connected.")
-  )
+  .then(() => console.log("MongoDB Database on Mongo Atlas Cloud Service Connected."))
   .catch((err) => console.log(`Database connection failed: ${err.message}`));
